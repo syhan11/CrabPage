fd1b2cf2cf6574f35bb9a133f35f21b778041573
syhan11 <sueyoung.6311@gmail.com>
2019-07-08 11:23:13 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Crab Cake Recipe" 
---
+++ src/test/java/com/example/crabpage/CrabpageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.crabpage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrabpageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
